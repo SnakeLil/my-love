d4feb1fbc2eb3a99408b5dbfee968dccb3f12f09
huangjiaqi <lilsnake239299@gmail.com>
2025-02-16 00:07:25 +0800

    feat: add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/love");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-xl">fanxin の 网站 ~~</h1>
         <a href="/happy-birthday/index.html">
