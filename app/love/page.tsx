3927c2c7821e6579aa6bbc362639f9ae2285cff0
huangjiaqi <lilsnake239299@gmail.com>
2025-02-15 22:51:38 +0800

    feat: add seo

--- app/love/page.tsx
+++ app/love/page.tsx
@@ -1,4 +1,14 @@
 import LovePage from "./components";
+export async function generateMetadata() {
+  const title = "可以成为我的恋人吗？";
+  const seo = {
+    title,
+    description: "可以成为我的恋人吗？窝 !喜欢你!! ( >᎑<)♡︎ᐝ",
+  };
+  return {
+    ...seo,
+  };
+}
 
 export default async function Page() {
   return (
